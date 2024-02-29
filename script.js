f8d8dbeec2a9575703bad0f5278556b208a12d3c
Gyan-max <76723725+Gyan-max@users.noreply.github.com>
2024-02-29 23:47:21 +0530

    modified:   index.html modified:   script.js modified:   style.css

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 let selections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
+let navLinks = document.querySelectorAll('header nav ul li a');
 
 window.onscroll = () => {
     sections.forEach(sec => {
@@ -13,7 +13,7 @@ window.onscroll = () => {
         if (top >= offset && top < offset + height) {
             navLinks.forEach(links => {
                 links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+                document.querySelector('header nav ul li a[href*=' + id + ']').classList.add(active);
             })
         }
     })
